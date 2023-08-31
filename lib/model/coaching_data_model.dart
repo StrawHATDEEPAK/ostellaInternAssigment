1715bd862710df1cc3003cbece6a156ea4d4a65b
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-08-31 16:54:04 +0530

    Created CoachingDataModel

---
+++ lib/model/coaching_data_model.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class CoachingDataModel {
+  final String name;
+  final String address;
+  final String rating;
+  final List<String> subjects;
+  final String image;
+  final int mutualFriendsStudyHere;
+  final double distanceFromUser;
+  final String offer;
+  CoachingDataModel({
+    required this.name,
+    required this.address,
+    required this.rating,
+    required this.subjects,
+    required this.image,
+    required this.mutualFriendsStudyHere,
+    required this.distanceFromUser,
+    required this.offer,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'address': address,
+      'rating': rating,
+      'subjects': subjects,
+      'image': image,
+      'mutualFriendsStudyHere': mutualFriendsStudyHere,
+      'distanceFromUser': distanceFromUser,
+      'offer': offer,
+    };
+  }
+
+  factory CoachingDataModel.fromMap(Map<String, dynamic> map) {
+    return CoachingDataModel(
+      name: map['name'] ?? '',
+      address: map['address'] ?? '',
+      rating: map['rating'] ?? '',
+      subjects: List<String>.from(map['subjects']),
+      image: map['image'] ?? '',
+      mutualFriendsStudyHere: map['mutualFriendsStudyHere']?.toInt() ?? 0,
+      distanceFromUser: map['distanceFromUser']?.toDouble() ?? 0.0,
+      offer: map['offer'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CoachingDataModel.fromJson(String source) =>
+      CoachingDataModel.fromMap(json.decode(source));
+}
